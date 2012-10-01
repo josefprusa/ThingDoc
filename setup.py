ef93996d26e722e23b84b430b2ea0eabf67aae96
Jakub Vysoky <jakub@borka.cz>
2012-10-01 12:05:12 +0200

    setuptools files so we can push pypi

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from os import path
+
+f = open(path.join(path.dirname(__file__), 'README'))
+long_description = f.read().strip()
+f.close()
+
+f = open(path.join(path.dirname(__file__), 'requirements.txt'))
+install_requires = f.read().strip()
+f.close()
+
+setup(
+    name='ThingDoc',
+    version='1.0',
+    description='ThingDoc is a clever things comment parser.',
+    long_description=long_description,
+    license='GNU GPLv3',
+    author='Josef Prusa',
+    author_email='iam@josefprusa.cz',
+    url='https://github.com/josefprusa/ThingDoc',
+    packages = ['thingdoc'],
+    scripts=['bin/thingdoc'],
+    install_requires=install_requires,
+)
+
